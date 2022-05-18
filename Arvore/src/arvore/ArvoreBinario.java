9aaf395bbca2ed0094335bfd9f3ca501ad890a82
Renan Carneiro Batista <92395163+Renan-Carneiro-Batista@users.noreply.github.com>
2022-05-18 08:24:13 -0300

    Apresentação sobre árvore em java

---
+++ Arvore/src/arvore/ArvoreBinario.java
@@ -0,0 +1,75 @@
+package arvore;
+
+public class ArvoreBinario {
+	private NoBinario root;
+	
+	public ArvoreBinario() {
+		super();
+		this.root = new NoBinario("raiz");
+	}
+
+	public ArvoreBinario(NoBinario root) {
+		super();
+		if (root == null) {
+			this.root = new NoBinario("raiz");
+		} else {
+			this.root = root;
+		}
+	}
+
+	public void addNo(String Add, String posicao, NoBinario noPai) {
+		NoBinario noToAdd = new NoBinario(Add);
+		if (noPai == null) {
+			addFilho(noToAdd, posicao, root);
+		} else {
+			addFilho(noToAdd, posicao, noPai);
+		}
+	}
+	
+	public void removeNo(String posicao, NoBinario noPai) {
+		if(posicao == "direita") {
+			removeFilho("direita", root);
+		}else {
+			removeFilho("esquerda", root);
+		}
+	}
+	
+	private void addFilho(NoBinario noToAdd, String posicao, NoBinario pai) {
+		if (posicao.equals("direita")) {
+			pai.setDireita(noToAdd);
+		} else {
+			pai.setEsquerda(noToAdd);
+		}
+	}
+	
+	private void removeFilho(String posicao, NoBinario pai) {
+		if (posicao.equals("direita")) {
+			pai.setDireita(null);
+		} else {
+			pai.setEsquerda(null);
+		}
+	}
+	
+	public void buscaPreOrdem(NoBinario no) {
+		System.out.print(no.getDescricao());
+		
+		if(no.getEsquerda() != null) {
+			buscaPreOrdem(no.getEsquerda());
+		}
+		if(no.getDireita() != null) {
+			buscaPreOrdem(no.getDireita());
+		}
+		
+	}
+
+	public NoBinario getRoot() {
+		return root;
+	}
+
+	public void setRoot(NoBinario root) {
+		this.root = root;
+	}
+	
+	
+	
+}
---
+++ Arvore/src/arvore/HelloArvore.java
@@ -0,0 +1,16 @@
+package arvore;
+
+public class HelloArvore {
+
+	public static void main(String[] args) {
+		
+		ArvoreBinario arvoreBinario = new ArvoreBinario();
+		arvoreBinario.addNo("folha1", "esquerda", null);
+		arvoreBinario.addNo("folha2", "direita", null);
+		
+		arvoreBinario.removeNo("esquerda", arvoreBinario.getRoot());
+		arvoreBinario.removeNo("direita", arvoreBinario.getRoot());
+		
+		arvoreBinario.buscaPreOrdem(arvoreBinario.getRoot());
+	}
+}
